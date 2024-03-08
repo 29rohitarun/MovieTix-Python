7f684f2560e03e16c78a00d90f5771a11283f68c
rarun3 <rarun3@ncsu.edu>
2024-03-08 17:09:50 -0500

    removed debug mode

--- FLASK_SITE.py
+++ FLASK_SITE.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def chatbot():
+    #return html 
     return render_template('TIX.html')
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
